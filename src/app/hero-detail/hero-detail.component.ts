e82d876cb228ccfbaea0a3bb3f77b2749a3a4daa
Jesus Bautista <jesus.gudino@wgu.edu>
2022-05-23 15:11:18 -0500

    tutorial complete

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+import {ActivatedRoute} from "@angular/router";
+import {HeroService} from "../hero.service";
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+
+  @Input() hero?: Hero;
+  heroes: Hero[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+}
